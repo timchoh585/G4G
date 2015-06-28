0ea40ff5d3857f795bd672d406f093e2583bfde0
Chris Bohl <chrisrbohl@gmail.com>
2015-06-28 07:49:34 -0500

    fixing positioning

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -1,7 +1,6 @@
 class TreesController < ApplicationController
   def show
     @tree = Tree.find_by(id: params[:id])
-    puts "************ #{@tree}"
     @donors = @tree.sort_donors_chron
     @charity = @tree.charity
   end
