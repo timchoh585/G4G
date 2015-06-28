d71bac95050ca746523b6588db2b691aebc9e978
timchoh585 <timchoh@gmail.com>
2015-06-28 04:17:35 -0500

    fixing index and single whatever

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class DashboardController < ApplicationController
   def index
+  	# get "/" do
+  	# 	erb :"dashboard/index"
+  	# end
+  	# get "/single" do
+  	# 	erb :"dashboard/single"
+  	# end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :dashboard
 
   get 'givegood' => 'givegood#index'
+  get 'single' => 'dashboard#single'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
