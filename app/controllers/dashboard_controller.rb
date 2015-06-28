882f0b088d2cfe5fe59f3dd945e46f1430301930
timchoh585 <timchoh@gmail.com>
2015-06-28 04:49:20 -0500

    fixed merge problems

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,14 +1,4 @@
 class DashboardController < ApplicationController
   def index
-<<<<<<< HEAD
-  	# get "/" do
-  	# 	erb :"dashboard/index"
-  	# end
-  	# get "/single" do
-  	# 	erb :"dashboard/single"
-  	# end
-=======
-
->>>>>>> 49ea453b26f5e8429a37a8076ca8ff657dc7e24d
   end
 end
